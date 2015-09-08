dc340de1935fb0d26b04064230dc4add53f19208
Dan Rovito <DanRovito@users.noreply.github.com>
2015-09-07 23:22:27 -0400

    Update class_gravatar.php

--- class_gravatar.php
+++ class_gravatar.php
@@ -6,7 +6,9 @@
  * @author Dan Rovito <danrovito.com>
  * @version 1
  */
-trait gravatario
+namespace DanRovito\Gravatario
+use Carbon\Carbon;
+trait Gravatario
 {
 	public function getGravatarAttribute()
 	{
