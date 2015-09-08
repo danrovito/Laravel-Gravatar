065b8ce843768920f52cf9ec79fd5797b77e0b6c
Dan Rovito <DanRovito@users.noreply.github.com>
2015-09-07 22:33:57 -0400

    Update class_gravatar.php

--- class_gravatar.php
+++ class_gravatar.php
@@ -8,5 +8,9 @@
  */
 class gravatar
 {
-
+  public function getGravatarAttribute()
+	{
+    	$hash = md5(strtolower(trim($this->attributes['email'])));    	
+    	return "https://secure.gravatar.com/avatar/$hash?d=mm";
+	}
 }
